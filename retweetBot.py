1fd091a3f6db0c2f5b9f455d5e9eeae1c211562d
Tochukwu <tochukwusage4@gmail.com>
2020-10-12 03:49:31 +0100

    retweet Bot

---
+++ retweetBot.py
@@ -0,0 +1,30 @@
+import tweepy
+import time
+import os
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = 'SARSMUSTEND'
+nrweets = 500
+
+for tweet in tweepy.Cursor(api.search, search).items(nrweets):
+    try:
+        print('Tweet Retweeted')
+        tweet.retweet()
+        time.sleep(45)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
