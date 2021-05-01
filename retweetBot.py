c857bdb4777299d0b28dcee90dbea98dbf7ff3e6
toc-dev <60432608+toc-dev@users.noreply.github.com>
2021-05-01 14:58:05 +0100

    Update retweetBot.py

--- retweetBot.py
+++ retweetBot.py
@@ -16,7 +16,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'ENDSARS'
+search = 'machinelearningmaster'
 nrweets = 500
 
 for tweet in tweepy.Cursor(api.search, search).items(nrweets):
