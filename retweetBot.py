1265d086e6d4dae3a5b6afacfa939514d4794b45
Tochukwusage <60432608+toc-dev@users.noreply.github.com>
2022-01-01 01:00:13 +0100

    Update retweetBot.py

--- retweetBot.py
+++ retweetBot.py
@@ -16,7 +16,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'machinelearningmaster'
+search = 'happy new year'
 nrweets = 500
 
 for tweet in tweepy.Cursor(api.search, search).items(nrweets):
