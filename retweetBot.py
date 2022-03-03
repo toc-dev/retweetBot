2638b5cf8e3af3d32bf0058cb6d144a16b884f08
Tochukwusage <60432608+toc-dev@users.noreply.github.com>
2022-03-03 15:18:39 +0100

    Update retweetBot.py

--- retweetBot.py
+++ retweetBot.py
@@ -16,14 +16,14 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'masterofmachines'
+search = 'wisdom is a weapon'
 nrweets = 500
 
 for tweet in tweepy.Cursor(api.search, search).items(nrweets):
     try:
         print('Tweet Retweeted')
         tweet.retweet()
-        time.sleep(45000)
+        time.sleep(450)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
